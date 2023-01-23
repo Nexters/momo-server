3fb87880af674c110c85f7a708f819147d1a0bbd
CHO Min HO <chominho96@naver.com>
2023-01-24 00:24:53 +0900

    feat: Session 엔티티 생성
    1. Session 엔티티 생성
    
    ** 전번 커밋에서 SessionStatus 클래스 생성 및 해당 커밋에서 Session 엔티티 생성

---
+++ src/main/java/com/nexters/momo/session/domain/Session.java
@@ -0,0 +1,73 @@
+package com.nexters.momo.session.domain;
+
+import com.nexters.momo.session.dto.PostSessionReqDTO;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * 각 주차 세션을 나타내는 Session 엔티티입니다.
+ * TODO : Generation 엔티티 연관 관계 매핑
+ *
+ * @author CHO Min Ho
+ */
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sess_id")
+    private Long id;
+
+    @Column(name = "sess_title")
+    private String title;
+
+    @Column(name = "start_at")
+    private LocalDateTime sessionStartTime;
+
+    @Column(name = "end_at")
+    private LocalDateTime sessionEndTime;
+
+    @Column(name = "attd_code")
+    private Integer attendanceCode;
+
+    @Column(name = "publish_at")
+    private LocalDateTime sessionPublishTime;
+
+    @Column(name = "sess_status")
+    @Enumerated(EnumType.STRING)
+    private SessionStatus status;
+
+    @Column(name = "sess_addr", columnDefinition = "TEXT")
+    private String sessionAddress;
+
+    private Session(String title, LocalDateTime sessionStartTime, LocalDateTime sessionEndTime, String sessionAddress) {
+        this.title = title;
+        this.sessionStartTime = sessionStartTime;
+        this.sessionEndTime = sessionEndTime;
+        this.attendanceCode = generateAttendanceCode();
+        this.sessionPublishTime = LocalDateTime.now();
+        this.status = SessionStatus.BEFORE;
+        this.sessionAddress = sessionAddress;
+    }
+
+    /**
+     * Session 엔티티를 생성하는 static 메서드입니다.
+     * Session 엔티티는 해당 메서드를 이용해서만 생성됩니다.
+     * @return 생성된 session 엔티티
+     */
+    public static Session createSession(PostSessionReqDTO dto) {
+        return new Session(dto.getTitle(), dto.getStartAt(), dto.getEndAt(), dto.getSessionAddress());
+    }
+
+    /**
+     * 출석코드 (랜덤 정수 3자리)를 생성합니다.
+     * @return 생성된 3자리 랜덤 코드
+     */
+    private int generateAttendanceCode() {
+        return (int)(Math.random() * (999 - 100 + 1)) + 100;
+    }
+}
