d8f99a4a188d73e0659885cf0fc51ab34dba07db
jiwoo <zbqmgldjfh@gmail.com>
2023-01-23 14:44:34 +0900

    test: Member Password 테스트
    비밀번호 일치, 불일치 테스트 작성

--- src/test/kotlin/com/nexters/momo/member/domain/MemberTest.java
+++ src/test/kotlin/com/nexters/momo/member/domain/MemberTest.java
@@ -1,5 +1,6 @@
 package com.nexters.momo.member.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +13,25 @@ class MemberTest {
     public void create_member() {
         assertThatCode(() -> new Member("unique_id", "password", "shine", "010-1234-5678", Role.USER, true))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("멤버 비밀번호 일치 테스트")
+    @Test
+    public void member_password_match_test() {
+        // given
+        Member member = new Member("unique_id", "password", "shine", "010-1234-5678", Role.USER, true);
+
+        // when, then
+        Assertions.assertThat(member.isSamePassword("password")).isTrue();
+    }
+
+    @DisplayName("멤버 비밀번호 불일치 테스트")
+    @Test
+    public void member_password_not_match_test() {
+        // given
+        Member member = new Member("unique_id", "password", "shine", "010-1234-5678", Role.USER, true);
+
+        // when, then
+        Assertions.assertThat(member.isSamePassword("invalid-password")).isFalse();
     }
 }
