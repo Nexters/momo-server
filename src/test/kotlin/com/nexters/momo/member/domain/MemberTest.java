c19f9d19cdc1d2e007c838caf84b463f7a6c2f02
jiwoo <zbqmgldjfh@gmail.com>
2023-01-23 15:04:20 +0900

    test: Member Name 생성 테스트
    이름의 길이는 0글자 부터 20글자 까지만 가능하도록 검증 테스트 작성

--- src/test/kotlin/com/nexters/momo/member/domain/MemberTest.java
+++ src/test/kotlin/com/nexters/momo/member/domain/MemberTest.java
@@ -1,8 +1,11 @@
 package com.nexters.momo.member.domain;
 
+import com.nexters.momo.member.exception.InvalidUserNameException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
 
@@ -33,5 +36,21 @@ class MemberTest {
 
         // when, then
         Assertions.assertThat(member.isSamePassword("invalid-password")).isFalse();
+    }
+
+    @DisplayName("이름은 1글자 부터 20글자 까지 가능하다")
+    @ParameterizedTest
+    @ValueSource(strings = {"김", "김23456789김23456789"})
+    public void member_name_length_test(String name) {
+        assertThatCode(() -> new Member("unique_id", "password", name, "010-1234-5678", Role.USER, true))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("이름이 0글자 이거나 20글자 초과시 생성에 실패한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "김123456789김123456789김"})
+    public void member_name_invalid_length_test(String name) {
+        Assertions.assertThatThrownBy(() -> new Member("unique_id", "password", name, "010-1234-5678", Role.USER, true))
+                        .isInstanceOf(InvalidUserNameException.class);
     }
 }
