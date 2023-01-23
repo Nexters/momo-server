37224cc314e182148cc9649e58cdba3852e0231d
jiwoo <zbqmgldjfh@gmail.com>
2023-01-23 14:25:11 +0900

    feat: Member Entity 생성 완료

---
+++ src/main/java/com/nexters/momo/member/domain/Member.java
@@ -0,0 +1,66 @@
+package com.nexters.momo.member.domain;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.CollectionTable;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "update member set deleted = true where member_id = ?")
+@Where(clause = "deleted = false")
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(name = "unique_id", length = 64, nullable = false)
+    private String uniqueId;
+
+    @Column(length = 64, nullable = false)
+    private String password;
+
+    @Column(length = 16, nullable = false)
+    private String name;
+
+    private String phone;
+
+    @Enumerated(value = EnumType.STRING)
+    private Role role;
+
+    @Column(name = "member_status")
+    @Enumerated(value = EnumType.STRING)
+    private MemberStatus memberStatus = MemberStatus.ACTIVE;
+
+    @Column(name = "private_policy_agreed")
+    private Boolean privatePolicyAgreed = Boolean.FALSE;
+
+    @ElementCollection
+    @CollectionTable(name = "team", joinColumns = @JoinColumn(name = "member_id"))
+    @Column(name = "team_id")
+    private Set<Long> teams = new HashSet<>();
+
+    public Member(String uniqueId, String password, String name, String phone, Role role, Boolean privatePolicyAgreed) {
+        this.uniqueId = uniqueId;
+        this.password = password;
+        this.name = name;
+        this.phone = phone;
+        this.role = role;
+        this.privatePolicyAgreed = privatePolicyAgreed;
+    }
+}
---
+++ src/main/java/com/nexters/momo/member/domain/Role.java
@@ -0,0 +1,14 @@
+package com.nexters.momo.member.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Role {
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN"),
+    ANONYMOUS("ROLE_ANONYMOUS");
+
+    private final String role;
+}
