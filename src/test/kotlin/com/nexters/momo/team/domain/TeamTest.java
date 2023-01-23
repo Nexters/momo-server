8fbb2e0dea8d680b6e50b33005303ab8f23b3220
jiwoo <zbqmgldjfh@gmail.com>
2023-01-23 16:25:15 +0900

    test: Team 이름 길이 검증 테스트 작성

--- src/test/kotlin/com/nexters/momo/team/domain/TeamTest.java
+++ src/test/kotlin/com/nexters/momo/team/domain/TeamTest.java
@@ -1,9 +1,13 @@
 package com.nexters.momo.team.domain;
 
+import com.nexters.momo.team.exception.InvalidTeamNameException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("도메인 : Team")
 class TeamTest {
@@ -13,5 +17,21 @@ class TeamTest {
     public void create_team() {
         assertThatCode(() -> new Team("OVN", 1234L))
                 .doesNotThrowAnyException();
+    }
+
+    @DisplayName("팀 이름은 1글자 부터 30글자 까지 가능하다")
+    @ParameterizedTest
+    @ValueSource(strings = {"팀", "팀23456789팀23456789팀23456789"})
+    public void team_name_length_test(String teamName) {
+        assertThatCode(() -> new Team(teamName, 1234L))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("팀 이름이 0글자 이거나 30글자 초과시 생성에 실패한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"", "팀123456789팀123456789팀123456789팀"})
+    public void team_name_invalid_length_test(String teamName) {
+        assertThatThrownBy(() -> new Team(teamName, 1234L))
+                .isInstanceOf(InvalidTeamNameException.class);
     }
 }
