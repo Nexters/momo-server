39ff9f637bf1d166ce074af8a879ba9950ef859d
BBBOMi <k.bomi096@gmail.com>
2023-01-23 16:31:38 +0900

    setting: kotlin에서 java로 변경

---
+++ src/test/java/com/nexters/momo/MomoServerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.nexters.momo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MomoServerApplicationTests {
+
+    @Test
+    public void contextLoads() { }
+}
