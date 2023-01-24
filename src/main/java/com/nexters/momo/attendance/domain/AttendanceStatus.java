d3bcb10c4217b9ec6f134780ba2ca6b4989eee2c
CHO Min HO <chominho96@naver.com>
2023-01-24 15:15:58 +0900

    feat: AttendanceStatus 클래스 작성
    1. 출석 상태를 나타내는 AttendanceStatus 클래스 작성

---
+++ src/main/java/com/nexters/momo/attendance/domain/AttendanceStatus.java
@@ -0,0 +1,12 @@
+package com.nexters.momo.attendance.domain;
+
+/**
+ * 출석 상태를 나타내는 enum 클래스입니다.
+ *
+ * @author CHO Min Ho
+ */
+public enum AttendanceStatus {
+    ABSENT,
+    ATTENDANCE,
+    LATE
+}
