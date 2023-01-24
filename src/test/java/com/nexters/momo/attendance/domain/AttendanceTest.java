0cf2b756ff994dc4c699a444a4d2927ed0799dc8
CHO Min HO <chominho96@naver.com>
2023-01-24 15:43:08 +0900

    test: 출석 코드 불일치 테스트 작성
    1. 출석 코드 불일치 테스트 작성

--- src/test/java/com/nexters/momo/attendance/domain/AttendanceTest.java
+++ src/test/java/com/nexters/momo/attendance/domain/AttendanceTest.java
@@ -1,7 +1,9 @@
 package com.nexters.momo.attendance.domain;
 
+import com.nexters.momo.attendance.exception.InvalidAttendanceCodeException;
 import com.nexters.momo.session.domain.Session;
 import com.nexters.momo.session.dto.PostSessionReqDTO;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
@@ -9,6 +11,7 @@ import java.time.LocalDateTime;
 import static com.nexters.momo.attendance.domain.AttendanceStatus.ATTENDANCE;
 import static com.nexters.momo.session.domain.Session.createSession;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -40,6 +43,24 @@ class AttendanceTest {
         // then
         assertThat(attendance.getStatus()).isEqualTo(ATTENDANCE);
 
+    }
+
+    @DisplayName("출석 코드 불일치 테스트")
+    @Test
+    void invalidAttendanceCodeTest() {
+        // given
+        LocalDateTime sessionStartTime = LocalDateTime.now().plusMinutes(10);
+        LocalDateTime sessionEndTime = LocalDateTime.now().plusMinutes(100);
+
+        session = createSession(new PostSessionReqDTO("세션 제목", sessionStartTime, sessionEndTime,
+                "서울시 강남구"));
+
+        attendanceCode = session.getAttendanceCode();
+
+        // when, then
+        assertThatThrownBy(() ->
+                Attendance.createAttendance(session, attendanceCode + 1)).
+                isInstanceOf(InvalidAttendanceCodeException.class);
     }
 
 
