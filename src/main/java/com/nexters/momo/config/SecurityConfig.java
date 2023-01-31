2d472dfc7f9627be31b8199f6f9cca4161ccf501
jiwoo <zbqmgldjfh@gmail.com>
2023-01-31 15:02:46 +0900

    feat: PasswordEncoder빈 등록

--- src/main/java/com/nexters/momo/config/SecurityConfig.java
+++ src/main/java/com/nexters/momo/config/SecurityConfig.java
@@ -1,10 +1,13 @@
 package com.nexters.momo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,5 +23,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .and()
                 .authorizeRequests()
                 .anyRequest().permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
