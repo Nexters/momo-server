d38585393356832d0d0663a70cd6ff7eeb92d1cc
jiwoo <zbqmgldjfh@gmail.com>
2023-01-31 14:13:44 +0900

    feat: 인수 테스트간의 격리환경 구축

---
+++ src/test/java/com/nexters/momo/acceptance/AcceptanceTest.java
@@ -0,0 +1,24 @@
+package com.nexters.momo.acceptance;
+
+import com.nexters.momo.testdata.DatabaseCleanup;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    private DatabaseCleanup databaseCleanup;
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.port = port;
+        databaseCleanup.execute();
+    }
+}
