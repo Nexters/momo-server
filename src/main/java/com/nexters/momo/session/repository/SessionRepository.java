17dee6af9aae6824110829b0d39abfd0be6873a6
CHO Min HO <chominho96@naver.com>
2023-02-07 17:14:01 +0900

    feat: JPA Session repository 추가

---
+++ src/main/java/com/nexters/momo/session/repository/SessionRepository.java
@@ -0,0 +1,15 @@
+package com.nexters.momo.session.repository;
+
+import com.nexters.momo.session.domain.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Session JPA Repository 입니다.
+ *
+ * @author CHO Min Ho
+ */
+public interface SessionRepository extends JpaRepository<Session, Long> {
+    List<Session> getSessionByGenerationId(Long generationId);
+}
