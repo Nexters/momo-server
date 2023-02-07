a385fca656d8f5e4d75c965a934c5ad1d25fee49
CHO Min HO <chominho96@naver.com>
2023-02-07 17:17:53 +0900

    feat: Session Controller 추가
    1. Session CRUD 관련 컨트롤러 추가

--- src/main/java/com/nexters/momo/common/response/ResponseCodeAndMessages.java
+++ src/main/java/com/nexters/momo/common/response/ResponseCodeAndMessages.java
@@ -6,7 +6,17 @@ public enum ResponseCodeAndMessages {
 
     /* MEMBER */
     MEMBER_CREATE_SUCCESS(HttpStatus.CREATED.value(), "유저 생성에 성공했습니다"),
-    USER_LOGIN_SUCCESS(HttpStatus.OK.value(), "로그인에 성공했습니다.");
+    USER_LOGIN_SUCCESS(HttpStatus.OK.value(), "로그인에 성공했습니다."),
+
+    /* USER */
+    USER_CREATE_SUCCESS(HttpStatus.CREATED.value(), "유저 생성에 성공했습니다."),
+
+    /* SESSION */
+    SESSION_SINGLE_READ_SUCCESS(HttpStatus.OK.value(), "단일 세션 조회에 성공했습니다."),
+    SESSION_CREATE_SUCCESS(HttpStatus.CREATED.value(), "세션 생성에 성공했습니다."),
+    SESSION_UPDATE_SUCCESS(HttpStatus.OK.value(), "세션 수정에 성공했습니다."),
+    SESSION_LIST_READ_SUCCESS(HttpStatus.OK.value(), "세션 리스트 조회에 성공했습니다."),
+    SESSION_DELETE_SUCCESS(HttpStatus.OK.value(), "세션 삭제에 성공했습니다");
 
     private final int code;
     private final String message;
