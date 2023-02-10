3efb2f1d6d649d23f154c6a444ec16e71a464cd3
BBBOMi <k.bomi096@gmail.com>
2023-02-10 21:13:57 +0900

    feat: 기수 엔티티 필드 수정

--- src/main/java/com/nexters/momo/generation/domain/Generation.java
+++ src/main/java/com/nexters/momo/generation/domain/Generation.java
@@ -17,37 +17,30 @@ public class Generation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long generationId;
+
+    @Column(nullable = false)
+    private int number;
 
     @Embedded
     private SignupCode signupCode;
 
     @Column(nullable = false)
-    private LocalDateTime startedAt;
-
-    @Column(nullable = false)
-    private LocalDateTime endedAt;
-
-    @Column(nullable = false)
-    private LocalDateTime createdAt;
+    private boolean active;
 
     public static Generation of(
             Long id,
+            int number,
             SignupCode signupCode,
-            LocalDateTime startedAt,
-            LocalDateTime endedAt,
-            LocalDateTime createdAt
+            boolean active
     ) {
         Assert.notNull(signupCode, "signupCode must not be null");
-        Assert.notNull(startedAt, "startedAt must not be null");
-        Assert.notNull(endedAt, "endedAt must not be null");
 
-        return new Generation(id, signupCode, startedAt, endedAt, createdAt);
+        return new Generation(id, number, signupCode, active);
     }
 
     public boolean isActive() {
-        LocalDateTime now = LocalDateTime.now();
-        return this.startedAt.isBefore(now) && this.endedAt.isAfter(now);
+        return active;
     }
 
 }
