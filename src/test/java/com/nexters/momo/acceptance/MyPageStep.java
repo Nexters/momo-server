51bbc072713f06add75bffc0bbf9402ce0507416
jiwoo <zbqmgldjfh@gmail.com>
2023-02-15 15:12:53 +0900

    test: 회원 정보 조회 인수테스트 작성

---
+++ src/test/java/com/nexters/momo/acceptance/MyPageStep.java
@@ -0,0 +1,31 @@
+package com.nexters.momo.acceptance;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+public class MyPageStep {
+
+    public static ExtractableResponse<Response> 베어러_인증으로_내_회원_정보_조회_요청(String accessToken) {
+        return RestAssured.given().log().all()
+                .auth().oauth2(accessToken)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .when().get("/api/mypages/me")
+                .then().log().all()
+                .statusCode(HttpStatus.OK.value())
+                .extract();
+    }
+
+    public static void 회원_정보_조회_확인(ExtractableResponse<Response> response, String email, String name) {
+        assertAll(
+                () -> assertThat(response.jsonPath().getBoolean("data.active")).isFalse(),
+                () -> assertThat(response.jsonPath().getString("data.name")).isEqualTo(name),
+                () -> assertThat(response.jsonPath().getString("data.email")).isEqualTo(email)
+        );
+    }
+}
