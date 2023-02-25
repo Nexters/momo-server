280d455ab6b20f73789a07d1430df54f73995d72
BBBOMi <k.bomi096@gmail.com>
2023-02-25 15:26:44 +0900

    setting: 응답 값 추가

--- src/main/java/com/nexters/momo/generation/presentation/GenerationApiSpec.java
+++ src/main/java/com/nexters/momo/generation/presentation/GenerationApiSpec.java
@@ -1,16 +1,41 @@
 package com.nexters.momo.generation.presentation;
 
+import com.nexters.momo.common.response.ErrorResponse;
 import com.nexters.momo.generation.presentation.dto.GenerationRequest;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 @Tag(name = "기수", description = "기수 API 목록")
 public interface GenerationApiSpec {
 
-    @Operation(summary = "기수 생성")
-    ResponseEntity<Void> create(GenerationRequest request);
+    @Operation(
+            summary = "기수 생성",
+            responses = {
+                    @ApiResponse(responseCode = "201",
+                            content = @Content(schema = @Schema())
+                    ),
+                    @ApiResponse(responseCode = "500",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    ),
+            }
+    )
+    ResponseEntity<Void> create(@RequestBody GenerationRequest request);
 
-    @Operation(summary = "현재 기수 비활성화")
+    @Operation(
+            summary = "현재 기수 비활성화",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            content = @Content(schema = @Schema())
+                    ),
+                    @ApiResponse(responseCode = "500",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    ),
+            }
+    )
     ResponseEntity<Void> deactivate(Long id);
 }
--- src/main/java/com/nexters/momo/member/mypage/presentation/UserApiSpec.java
+++ src/main/java/com/nexters/momo/member/mypage/presentation/UserApiSpec.java
@@ -1,14 +1,31 @@
 package com.nexters.momo.member.mypage.presentation;
 
+import com.nexters.momo.common.response.ErrorResponse;
 import com.nexters.momo.member.auth.domain.Member;
 import com.nexters.momo.member.mypage.common.dto.response.MemberLookUpResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 
 @Tag(name = "유저", description = "유저 API 목록")
 public interface UserApiSpec {
 
-    @Operation(summary = "유저 조회")
+    @Operation(
+            summary = "유저 조회",
+            responses = {
+                    @ApiResponse(responseCode = "200",
+                            content = @Content(schema = @Schema(implementation = MemberLookUpResponse.class))
+                    ),
+                    @ApiResponse(responseCode = "404",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    ),
+                    @ApiResponse(responseCode = "500",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))
+                    ),
+            }
+    )
     ResponseEntity<MemberLookUpResponse> searchMe(Member member);
 }
