298f7e49dd52a823ecddb0862a54bab5a4b88761
jiwoo <zbqmgldjfh@gmail.com>
2023-02-28 22:27:36 +0900

    feat: AttendanceQueryRepository 구현

--- src/main/java/com/nexters/momo/attendance/domain/AttendanceRepository.java
+++ src/main/java/com/nexters/momo/attendance/domain/AttendanceRepository.java
@@ -2,5 +2,5 @@ package com.nexters.momo.attendance.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+public interface AttendanceRepository extends JpaRepository<Attendance, Long>, AttendanceQueryRepository {
 }
