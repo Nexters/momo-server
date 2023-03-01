3c9498c271471b2e287d663f4c13ba98d674bc37
jiwoo <zbqmgldjfh@gmail.com>
2023-03-01 11:44:02 +0900

    test: 잘못된 가입코드로 가입하는 인수테스트 작성

--- src/main/java/com/nexters/momo/member/auth/exception/handler/UserExceptionHandler.java
+++ src/main/java/com/nexters/momo/member/auth/exception/handler/UserExceptionHandler.java
@@ -5,16 +5,17 @@ import com.nexters.momo.common.response.ErrorResponse;
 import com.nexters.momo.member.auth.exception.DuplicatedUserDeviceIdException;
 import com.nexters.momo.member.auth.exception.DuplicatedUserEmailException;
 import com.nexters.momo.member.auth.exception.InvalidUserEmailException;
+import com.nexters.momo.member.auth.presentation.AuthController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(basePackageClasses = AuthController.class)
 public class UserExceptionHandler {
 
-    @ExceptionHandler({InvalidUserEmailException.class, DuplicatedUserEmailException.class, DuplicatedUserDeviceIdException.class})
+    @ExceptionHandler({InvalidUserEmailException.class, DuplicatedUserEmailException.class, DuplicatedUserDeviceIdException.class, IllegalArgumentException.class})
     public ResponseEntity<ErrorResponse> invalidUserRegisterExceptionHandler(Exception exception) {
         log.error("[InvalidUserRegisterException] Handler calling", exception);
         return ResponseEntity.status(ErrorCodeAndMessages.MEMBER_CREATE_FAIL.getCode())
