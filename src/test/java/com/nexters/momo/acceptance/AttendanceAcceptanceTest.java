63c3402226f080fa8ce5e214fd7b8e85fc30c74e
jiwoo <zbqmgldjfh@gmail.com>
2023-03-01 11:03:55 +0900

    test: 인수테스트에서 Admin이 기수를 사전에 생성하도록 구현

--- src/test/java/com/nexters/momo/acceptance/AttendanceAcceptanceTest.java
+++ src/test/java/com/nexters/momo/acceptance/AttendanceAcceptanceTest.java
@@ -1,12 +1,18 @@
 package com.nexters.momo.acceptance;
 
+import com.nexters.momo.generation.domain.GenerationRepository;
+import com.nexters.momo.member.auth.domain.Member;
 import com.nexters.momo.member.auth.domain.MemberRepository;
+import com.nexters.momo.member.auth.domain.Occupation;
+import com.nexters.momo.member.auth.domain.Role;
 import com.nexters.momo.member.auth.presentation.dto.MemberRegisterRequest;
 import com.nexters.momo.support.RandomPortConfigure;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.io.IOException;
 
@@ -23,13 +29,29 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AttendanceAcceptanceTest extends RandomPortConfigure {
 
     @Autowired
-    private MemberRepository memberRepository;
+    MemberRepository memberRepository;
+
+    @Autowired
+    GenerationRepository generationRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
 
     @Override
     @BeforeEach
     public void setUp() {
         super.setUp();
-        this.memberRepository.deleteAll();
+
+        // admin 생성
+        String encodedPassword = passwordEncoder.encode("admin_password");
+        Member adminMember = new Member("admin@naver.com", encodedPassword, "admin", "admin_uuid", Role.ADMIN, Occupation.DEVELOPER);
+        memberRepository.save(adminMember);
+
+        // admin 로그인
+        var adminAccessToken = 로그인_되어_있음("admin@naver.com", "admin_password", "admin_uuid");
+
+        // admin의 기수 생성
+        기수_생성_요청(adminAccessToken, "signup_code");
     }
 
     /**
@@ -45,13 +67,10 @@ public class AttendanceAcceptanceTest extends RandomPortConfigure {
     public void session_attendance_test() throws IOException {
         // given
         사용자_가입_요청(new MemberRegisterRequest("shine@naver.com",
-                "password", "Shine", 22, "developer", "uuid"));
+                "password", "Shine", 22, "developer", "uuid", "signup_code"));
 
         // and
         var accessToken = 로그인_되어_있음("shine@naver.com", "password", "uuid");
-
-        // and
-        기수_생성_요청(accessToken);
 
         // and
         파일_전송과_함께_세션_생성_요청(accessToken);
@@ -67,5 +86,11 @@ public class AttendanceAcceptanceTest extends RandomPortConfigure {
 
         // then
         assertThat(출석_체크_요청_응답.statusCode()).isEqualTo(201);
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.memberRepository.deleteAll();
+        this.generationRepository.deleteAll();
     }
 }
--- src/test/java/com/nexters/momo/acceptance/MyPageAcceptanceTest.java
+++ src/test/java/com/nexters/momo/acceptance/MyPageAcceptanceTest.java
@@ -1,24 +1,60 @@
 package com.nexters.momo.acceptance;
 
+import com.nexters.momo.generation.domain.GenerationRepository;
+import com.nexters.momo.member.auth.domain.Member;
+import com.nexters.momo.member.auth.domain.MemberRepository;
+import com.nexters.momo.member.auth.domain.Occupation;
+import com.nexters.momo.member.auth.domain.Role;
 import com.nexters.momo.member.auth.presentation.dto.MemberRegisterRequest;
 import com.nexters.momo.support.RandomPortConfigure;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static com.nexters.momo.acceptance.AuthStep.로그인_되어_있음;
 import static com.nexters.momo.acceptance.AuthStep.사용자_가입_요청;
+import static com.nexters.momo.acceptance.GenerationStep.기수_생성_요청;
 import static com.nexters.momo.acceptance.MyPageStep.베어러_인증으로_내_회원_정보_조회_요청;
 import static com.nexters.momo.acceptance.MyPageStep.회원_정보_조회_확인;
 
 @DisplayName("인수 : 사용자 정보")
 public class MyPageAcceptanceTest extends RandomPortConfigure {
 
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    GenerationRepository generationRepository;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Override
+    @BeforeEach
+    public void setUp() {
+        super.setUp();
+
+        // admin 생성
+        String encodedPassword = passwordEncoder.encode("admin_password");
+        Member adminMember = new Member("admin@naver.com", encodedPassword, "admin", "admin_uuid", Role.ADMIN, Occupation.DEVELOPER);
+        memberRepository.save(adminMember);
+
+        // admin 로그인
+        var adminAccessToken = 로그인_되어_있음("admin@naver.com", "admin_password", "admin_uuid");
+
+        // admin의 기수 생성
+        기수_생성_요청(adminAccessToken, "signup_code");
+    }
+
     @DisplayName("사용자 본인의 프로필을 조회한다.")
     @Test
     public void find_member_info() {
         // given
         사용자_가입_요청(new MemberRegisterRequest("user@email.com",
-                "password", "shine", 22, "developer", "device_uuid"));
+                "password", "shine", 22, "developer", "device_uuid", "signup_code"));
 
         // when
         String accessToken = 로그인_되어_있음("user@email.com", "password", "device_uuid");
@@ -28,5 +64,11 @@ public class MyPageAcceptanceTest extends RandomPortConfigure {
 
         // then
         회원_정보_조회_확인(회원_정보_응답, "user@email.com", "shine");
+    }
+
+    @AfterEach
+    void tearDown() {
+        this.memberRepository.deleteAll();
+        this.generationRepository.deleteAll();
     }
 }
